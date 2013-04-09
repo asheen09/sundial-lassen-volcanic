b5ad6acb499504cfed1450cf6e6a01f57afcc299
galock4vr <galock4vr@yahoo.com>
2013-04-09 02:07:33 -1000

    Finished method for basic angle calculator, no adjustment by longitude yet

--- SundialCompute.java
+++ SundialCompute.java
@@ -1,9 +1,10 @@
 package Sundial;
 
+
 /**
  * double[] angOfHours;
  * 	Stores the angles made between the hour lines and the gnomon
- * 	position 0 = 12 am; position 12 = 12 pm
+ * 	position 0 = 12 pm; position 6 = 6 am
  */
 
 public class SundialCompute{
@@ -15,7 +16,7 @@ public class SundialCompute{
 	public SundialCompute(float lati, float longi){
 		latitude = lati;
 		longitude = longi;
-		angOfHours = new double[24];
+		angOfHours = new double[7];
 	}
 	
 	/**
@@ -24,12 +25,27 @@ public class SundialCompute{
 	 * tan(d) = tan(t)*sin(phi)
 	 * phi is the latitude of the place the sundial is located
 	 * d is the angle which the hour line makes with the gnomon
-    * t is the time measured from noon in degrees of arc.
+	 * t is the time measured from noon in degrees of arc. 1 hour = 15 degrees
 	 */
-	public void anglesFor(float phi){
+	public void hourAngles(){
 		double tanD = 0;
+		angOfHours[6] = 90;
 		
-		for(int i = 0; i < 24; i++){
+		for(int i = 0; i < 6; i++){
+			tanD = Math.tan((i*15.00/180)*Math.PI)*Math.sin((latitude/180)*Math.PI);
+			angOfHours[i] = Math.toDegrees(Math.atan(tanD));
 		} 	 	
+	}
+	
+	public void printAngles(){
+		for(int i = 0; i < 7; i++){
+			System.out.println((12-i) + ": " + angOfHours[i]);
+		}
+	}
+	
+	public static void main(String[] args){
+		SundialCompute sc = new SundialCompute(52, 100);
+		sc.hourAngles();
+		sc.printAngles();
 	}
 }
