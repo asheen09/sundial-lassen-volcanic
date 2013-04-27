f9af02c81550e27cd3f900421f9d86396d4abb20
galock4vr <galock4vr@yahoo.com>
2013-04-27 02:18:46 -1000

    Trying to merge modules.  Make sure everything is in package sundial

--- Sundial.java
+++ Sundial.java
@@ -1,10 +1,18 @@
-package Sundial;
+package sundial;
 
-public class SundialInterface{
-}
-class SundialCompute{
-}
-class SundialDraw{  
-}
+import java.applet.Applet;
+import java.awt.*;
 
-//how do you guys wanna set this up?
+public class Sundial extends Applet{
+	Panel container = new Panel();
+	SundialInterface sunInt = new SundialInterface();
+	
+	public void init(){
+		container.setLayout(new GridLayout(1,0));
+		container.add(sunInt);
+		add(container);
+		
+		sunInt.init();
+	
+	}
+}
